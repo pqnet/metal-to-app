d6c7b3f7e38f2c6e09eefd143b6df48d33d798c2
pqnet <pqnet@users.noreply.github.com>
2019-12-14 15:16:06 +0100

    implement freelist for frame allocator

--- src/frame.c
+++ src/frame.c
@@ -1,25 +1,40 @@
+#include <stddef.h>
+#include <stdalign.h>
 #include "frame.h"
 #include "interrupts.h"
+#include "memory.h"
 
-uintptr_t freemem_start;
-uintptr_t freemem_end; // first non-free address after last page
-uintptr_t freelist;
+linear_address freemem_start;
+linear_address freemem_end; // first non-free address after last page
 
-void init_frame_allocator(uintptr_t fm_start, uintptr_t fm_end) {
-    freemem_start = fm_start;
-    freemem_end = fm_end;
-    freelist = 0;
+alignas(0x1000)
+struct free_page {
+    struct free_page* nextFreePage;
+};
+struct free_page* freelist;
+
+void init_frame_allocator(linear_address fm_start, linear_address fm_end) {
+    freemem_start = ((fm_start + 0xfff) / 0x1000)* 0x1000;
+    freemem_end = (fm_end / 0x1000) * 0x1000;
+    freelist = NULL;
 }
 
-uintptr_t frame_alloc() {
+linear_address frame_alloc() {
+    if (freelist !== NULL) {
+        struct free_page* freePage = freelist;
+        freelist = freePage->nextFreePage;
+        return pointerToLinearAddres(freePage);
+    }
     if (freemem_start == freemem_end) {
         panic();
     }
-    uintptr_t ret = freemem_start;
+    linear_address ret = freemem_start;
     freemem_start += 0x1000;
     return ret;
 }
 
-void frame_dealloc(uintptr_t frame_address) {
-    // nop
+void frame_dealloc(linear_address frame_address) {
+    struct free_page* freePage = linearAddressToPtr(frame_address);
+    freePage->nextFreePage = freelist;
+    freelist = freePage;
 }
